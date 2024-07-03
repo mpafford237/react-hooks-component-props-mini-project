296c7c334816fd8ff08bc9806d06a795e1a84cac
masonPafford <mdpafford2015@gmail.com>
2024-07-03 17:21:33 -0500

    completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList({ posts }) {
+
+    const listArticles = posts.map((article) =>
+        <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>
+    )
+    return(
+        <main>
+            {listArticles}
+        </main>
+    )
+}
+
+export default ArticleList
